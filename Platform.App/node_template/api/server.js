bb7328b44703b7a9ed50ee318a6b2d6362b25a40
Phlippe Moneda <philippemoneda@gmail.com>
2018-01-02 11:37:27 -0200

    #8 adiciona a coluna default instance_id do tipo UUID (#11)
    * #8 adiciona a coluna default instance_id do tipo UUID
    
    * adiciona middleware para validar se foi passado o id da instancia no request
    
    * atualiza a documentação do Domain.App
    
    
    Adiciona a coluna de ID da instância em cada tabela;
    Para cada request é necessário passar um Header (Instance-Id) com o ID da instância do processo que está consumindo a API de domínio.
    
    O ID da instância deverá ser passado tanto para a API de leitura quanto de escrita.
    Caso não seja passado o ID será retornado um BadRequest

--- Platform.App/node_template/api/server.js
+++ Platform.App/node_template/api/server.js
@@ -6,15 +6,19 @@
 
 
 var restify = require('restify');
-var QueryController = require('./queryController.js');
-var SaveCommandController = require('./saveCommandController.js');
+var QueryController = require('./controllers/queryController.js');
+var SaveCommandController = require('./controllers/saveCommandController.js');
+var RequireInstanceIdMiddleware = require('./middlewares/requireInstanceId.js');
 var server = restify.createServer();
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
+server.use(RequireInstanceIdMiddleware);
+
 var query = new QueryController();
 var command = new SaveCommandController();
 server.get('/:appId/:entity', query.getEntityByAppId);
 server.post('/:appId/persist', command.persist);
+
 
 server.listen(9090, function() {
     console.log('%s listening at %s', server.name, server.url);
