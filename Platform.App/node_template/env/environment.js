3a2d3422750428d6abfa73e9fa1271c48e0f3efe
Philippe Moneda <philippemoneda@gmail.com>
2018-01-04 15:41:26 -0200

    configura a porta de subida da aplicação para pegar de uma configuração

--- Platform.App/node_template/env/environment.js
+++ Platform.App/node_template/env/environment.js
@@ -14,11 +14,13 @@ var fs = require("fs");
 
         var fileConfig = JSON.parse(fs.readFileSync("plataforma.json"));
         var cnf = {};
+        cnf.http = {};
         cnf.database = {};
         cnf.database.name = fileConfig.app.name;
         cnf.database.host = process.env.POSTGRES_HOST || "localhost";
         cnf.database.user = "postgres";
         cnf.database.password = "";
+        cnf.http.port = 9090;
         var p1 = new Promise(function(resolve, reject) {
             resolve(cnf);
         });
