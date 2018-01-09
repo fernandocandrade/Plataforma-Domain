da690382ce504d1da7c3d95c0b0eab0ef44f2dd3
Philippe Moneda <philippemoneda@gmail.com>
2018-01-09 18:11:25 -0200

    adiciona Dockerfile no cli da process app

--- Platform.Cli/actions/process/createAppAction.js
+++ Platform.Cli/actions/process/createAppAction.js
@@ -1,7 +1,10 @@
 const AppInstance = require("../../app_instance");
 const uuidv4 = require('uuid/v4');
 const BaseAction = require("../baseCreateAction");
-var shell = require('shelljs');
+var shell = require("shelljs");
+var fs = require("fs");
+
+
 module.exports = class CreateAppAction{
     constructor(){
         this.appInstance = new AppInstance();
@@ -15,6 +18,15 @@ module.exports = class CreateAppAction{
             shell.touch(path+"/process/"+plataforma.app.name+".js");
             shell.touch(path+"/metadados/EventCatalog.js");
             shell.touch(path+"/metadados/"+plataforma.app.name+".yaml");
+            
+            const Dockerfile = `
+FROM node:carbon
+WORKDIR /usr/src/${plataforma.app.name}
+COPY . .
+CMD [ "node", "process/${plataforma.app.name}.js" ]
+`
+            fs.writeFileSync(path+"/Dockerfile",new Buffer(Dockerfile),"UTF-8")
+            shell.touch(path+"/Dockerfile");
             
         }) 
     }     
