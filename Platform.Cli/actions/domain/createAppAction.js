d8af79d27294baf8aa0f96c4ff1be973f3e6c8a0
Philippe Moneda <philippemoneda@gmail.com>
2018-01-09 16:12:24 -0200

    salva os dados da domain app na database do api core

--- Platform.Cli/actions/domain/createAppAction.js
+++ Platform.Cli/actions/domain/createAppAction.js
@@ -1,5 +1,6 @@
 
 const AppInstance = require("../../app_instance");
+const uuidv4 = require('uuid/v4');
 module.exports = class CreateDomainAppAction{
     
 
@@ -24,12 +25,17 @@ module.exports = class CreateDomainAppAction{
             var name = answers["nome"];
             var path = process.cwd()+"/"+name;
             plataforma.app.name = name;
-            shell.mkdir('-p', path+'/Dominio',path+'/Migrations',path+'/Mapas');            
+            
             plataforma.app.version = answers["versao_plataforma"];
             plataforma.app.description = answers["descricao"];
             plataforma.app.author = answers["autor"];
             plataforma.solution = solution.solution;
-            fs.writeFileSync(path+"/plataforma.json",JSON.stringify(plataforma, null, 4),"UTF-8");
+
+            this.saveApiCore(plataforma,(id)=>{
+                plataforma.app.id = id;
+                shell.mkdir('-p', path+'/Dominio',path+'/Migrations',path+'/Mapas');
+                fs.writeFileSync(path+"/plataforma.json",JSON.stringify(plataforma, null, 4),"UTF-8");
+            })
         });
     }
 
@@ -65,7 +71,30 @@ module.exports = class CreateDomainAppAction{
         return questions;
      }
 
-     saveApiCore(){
-         
+     saveApiCore(platform,callback){
+        //TODO migrar para uma lib do Api Core
+        var unirest = require("unirest");
+        var req = unirest("POST", "http://localhost:9095/core/persist");
+        req.headers({
+            "content-type": "application/json",
+            "instance-id": "fe93a9a8-84d9-41ec-a056-e4606a72fbdd"
+        });
+        req.type("json");
+        var clone = JSON.parse(JSON.stringify(platform.app));
+        clone.systemId = platform.solution.id;
+        clone._metadata = {};
+        clone._metadata.type= "installedApps";
+        clone._metadata.changeTrack = "create";
+
+        req.send([clone]);
+        req.end(function (res) {
+            //caso nao esteja rodando a API Core
+            //entao executa "local"
+            if  (res.error){
+                callback(uuidv4())
+            }else{
+                callback(res.body[0].id);
+            }            
+        });
      }
 }
