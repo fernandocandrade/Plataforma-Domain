4ace5542d9011c13341e88a57a46f3cfd8fe4fa2
Philippe Moneda <philippemoneda@gmail.com>
2018-01-11 18:55:36 -0200

    adiciona parametro no-cache para gerar a imagem docker

--- Platform.Cli/services/docker.js
+++ Platform.Cli/services/docker.js
@@ -8,7 +8,7 @@ module.exports = class DockerService{
     build(env,tag){
         var promise = new Promise((resolve,reject)=>{
             try{
-                var cmd = `docker build . --tag ${tag} -q`
+                var cmd = `docker build . --tag ${tag} -q --no-cache`
                 var first = shell.exec(cmd).stdout.toString().split("sha256:")[1];
                 var imageId = first.replace("\n","");
                 resolve({imageId:imageId});
