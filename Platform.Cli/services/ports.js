8813f2e002b3f9dfb01904a30910174bf323aba8
Philippe Moneda <philippemoneda@gmail.com>
2018-01-12 09:45:26 -0200

    muda o cli para comecar a disponibilizar portas a partir da 9110

--- Platform.Cli/services/ports.js
+++ Platform.Cli/services/ports.js
@@ -2,20 +2,20 @@ var shell = require("shelljs");
 var os = require("os");
 var fs = require("fs");
 module.exports = class Ports{
-    
+
     path(){
         return os.tmpdir();
     }
     /**
-     * @description este método retorna uma próxima porta 
+     * @description este método retorna uma próxima porta
      * disponível para ser usada por uma aplicação
      */
     getNextAvailablePort(){
-        var port = 9100;
+        var port = 9110;
         var ports = this.getUsedPorts();
         if (ports.length > 0){
             return this.getUsedPorts().pop() + 1;
-        }        
+        }
         return port;
     }
 
