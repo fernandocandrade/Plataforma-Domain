0d174725bda85f5a96c556b34603088b0e656db9
Philippe Moneda <philippemoneda@gmail.com>
2018-01-12 13:14:55 -0200

    melhora o tratamento de erro para yamls invalidos

--- Platform.Cli/model_builder.js
+++ Platform.Cli/model_builder.js
@@ -1,13 +1,13 @@
 const Sequelize = require('sequelize');
 var Handlebars = require('handlebars');
 var root = __dirname +"/";
-Handlebars.registerHelper( "join", function( obj, sep, options ) {    
-    return Object.keys(obj).map(function( item ) {        
+Handlebars.registerHelper( "join", function( obj, sep, options ) {
+    return Object.keys(obj).map(function( item ) {
         return options.fn( obj[item] );
     }).join( sep );
 });
 
-Handlebars.registerHelper( "eq", function( lvalue, rvalue, options ) {           
+Handlebars.registerHelper( "eq", function( lvalue, rvalue, options ) {
     if (lvalue != rvalue) {
         return options.inverse(this)
     } else {
@@ -24,15 +24,15 @@ module.exports = (function(){
     self.model.relationships = [];
     self.model.seeds = {};
 
-    var keys = (model) => Object.keys(model)    
+    var keys = (model) => Object.keys(model)
     var hasValue = (array,value) => array.filter(v => v === value).length > 0;
 
     var isRelationship = (value) => ["hasMany","belongsTo","hasAndBelongsTo"].filter(v => v === value).length > 0;
     /**
-     * 
+     *
      * @param {JSON} model é o modelo definido no YAML no formato JSON
      * @description Este método carrega o modelo do YAML e verifica se ele já foi previamente
-     * carregado e adiciona na estrutra de dados de modelos que serão compilados na aplicação de 
+     * carregado e adiciona na estrutra de dados de modelos que serão compilados na aplicação de
      * dominio da plataforma
      */
     self.loadModel = function(model){
@@ -42,7 +42,7 @@ module.exports = (function(){
         }
         self.model.tables[name] = {};
         self.model.tables[name].columns = [];
-        
+
         var _model = self.model.tables[name];
         _model.tableName = name; //by default
         _model.columns = [];
@@ -51,18 +51,18 @@ module.exports = (function(){
             if (isRelationship(column)){
                 self.model.relationships.push([name,column,model[name][column][0]]);
                 return;
-            }            
+            }
             var modelColumn = {};
             modelColumn.name = column;
             var attrs = model[name][column];
             modelColumn.attributes = attrs;
-            self.model.tables[name].columns.push(modelColumn);            
-        });        
+            self.model.tables[name].columns.push(modelColumn);
+        });
     };
 
     /**
      * @description Compila o modelo de dados declarados no YAML
-     * para JavaScript/Sequelize através de um template definido 
+     * para JavaScript/Sequelize através de um template definido
      * dentro da pasta Platform.App/node_template/model/domain.tmpl
      */
     self.compile = (appName)=>{
@@ -73,13 +73,13 @@ module.exports = (function(){
 
         var source = fs.readFileSync(root+"node_template/model/domain.tmpl").toString();
         var template = Handlebars.compile(source);
-        var obj = {"database_name":appName, "model":self.sequelizeModel, "relations":self.model.relationships};        
+        var obj = {"database_name":appName, "model":self.sequelizeModel, "relations":self.model.relationships};
         var compiled = template(obj);
         return compiled;
     };
 
     /**
-     * 
+     *
      * @param {JSON} obj modelo de dominio na estrutura de dados pre compilacao
      * @param {Array<String>} array lista de atributos do dominio
      * @description Define o tipo da coluna já no formato do Sequelize
@@ -112,29 +112,34 @@ module.exports = (function(){
         "uuidV1":"Sequelize.UUIDV1",
         "uuidV4":"Sequelize.UUIDV4"
     };
-    
+
     self.sequelizeModel = {};
-    
+
     /**
-     * 
+     *
      * @param {JSON} table estrutura pre compilacao
-     * @description compila um objeto de dominio para ja considerando 
+     * @description compila um objeto de dominio para ja considerando
      * detalhes de chave ou atributos
      */
-    self.compileTable = (table)=>{        
-        var tableDefinition = {};              
+    self.compileTable = (table)=>{
+        var tableDefinition = {};
+        console.log(`Processing ${table.tableName}`);
+
         table.columns.forEach(c => {
             tableDefinition[c.name] = {};
             tableDefinition[c.name].name = c.name;
+            if(!Array.isArray(c.attributes)){
+                throw `Syntax error in file ${table.tableName} near to field ${c.name} attribute ${c.attributes}`;
+            }
             defineType(tableDefinition[c.name],c.attributes);
             if (hasValue(c.attributes,"primary key")){
                 tableDefinition[c.name].primaryKey = true;
             }
             if (hasValue(c.attributes,"auto increment")){
                 tableDefinition[c.name].autoIncrement = true;
-            }            
-        });  
-        self.sequelizeModel[table.tableName] = tableDefinition;      
+            }
+        });
+        self.sequelizeModel[table.tableName] = tableDefinition;
     };
     return self;
 })();
