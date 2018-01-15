2bd8c0fe1f901ca7c6933b4a489d5b622f471f20
Fabio Gibson <fabiogibson@hotmail.com>
2018-01-15 16:15:30 -0200

    tira a obrigatoriedade do InstanceId do Header

--- Platform.App/node_template/api/middlewares/requireInstanceId.js
+++ Platform.App/node_template/api/middlewares/requireInstanceId.js
@@ -8,12 +8,8 @@
  */
 module.exports = function(req,res,next){
     var instanceId = req.header("Instance-Id");
-    if (!instanceId){
-        res.send(400,{
-            message:"Instance Id not found"
-        });
-        return next;
+    if (instanceId){ 
+      req.instanceId = instanceId;
     }
-    req.instanceId = instanceId;
     return next();
 }
