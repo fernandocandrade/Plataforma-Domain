f356651a59bbf561f8080577d5ee43328d314a15
Philippe Moneda <philippemoneda@gmail.com>
2018-01-31 18:21:30 -0200

    mapper migrado e testado

--- Platform.App/python-template/mapper/index.py
+++ Platform.App/python-template/mapper/index.py
@@ -63,3 +63,84 @@ class Index(Component):
                     continue
                 proj = [field_obj['column'], field]
                 projections[mapped_model]['attributes'].append(proj)
+
+    def get_map_by_app_id(self, app_id):
+        """ returns cached map for an app_id """
+        if app_id in self.model_cache:
+            return self.model_cache[app_id]
+        return dict()
+
+    def get_map_by_app_id_and_name(self, app_id, name):
+        """ get map by app id and name """
+        if app_id in self.model_cache and name in self.model_cache[app_id]:
+            return self.model_cache[app_id][name]
+        else:
+            return dict()
+
+    def get_projection(self, app_id):
+        """ returns projection list of a app_id """
+        if app_id in self.projection_cache:
+            return self.projection_cache[app_id]
+        return dict()
+
+    def get_includes(self, app_id, map_name):
+        """ returns include objects """
+        if app_id in self.includes_map and map_name in self.includes_map[app_id]:
+            return self.includes_map[app_id][map_name]
+        return []
+
+    def get_filters(self, app_id, map_name):
+        """ returns filters on map  """
+        if self.has_model_cache(app_id, map_name) and "filters" in self.model_cache[app_id][map_name]:
+            return self.model_cache[app_id][map_name]["filters"]
+        return dict()
+
+    def get_fields(self, app_id, map_name):
+        """ returns map fields by app_id and name """
+        if self.has_model_cache(app_id, map_name) and "fields" in self.model_cache[app_id][map_name]:
+            return self.model_cache[app_id][map_name]["fields"]
+        return dict()
+
+    def get_model_name(self, app_id, map_name):
+        """ returns entity models name """
+        _map = self.get_map_by_app_id_and_name(app_id, map_name)
+        if 'model' in _map:
+            return _map['model']
+        return dict()
+
+    def get_functions(self, app_id, map_name):
+        """ returns function fields in map """
+        if self.has_functions_map(app_id, map_name):
+            return self.functions_map[app_id][map_name]
+        return dict()
+
+    def get_map_type_by_domain_type(self, app_id, domain_type):
+        """ returns mapped name based on domain name """
+        if self.has_entity_to_map(app_id, domain_type):
+            return self.entity_to_map[app_id][domain_type]
+        return ""
+
+    def columns_from_map_type(self, app_id, map_name):
+        """ return 1 to 1 map -> domain """
+        field_obj = self.model_cache[app_id][map_name]['fields']
+        _list = []
+        for map_column in field_obj:
+            if "column" in field_obj[map_column]:
+                reference = field_obj[map_column]['column']
+            if "type" in field_obj[map_column]:
+                reference = field_obj[map_column]['type']
+            if "include" in field_obj[map_column]:
+                reference = self.get_model_name(app_id, field_obj[map_column]['model'])
+            if reference == "function":
+                continue
+            _list.append([map_column, reference])
+        return _list
+
+    def has_entity_to_map(self, app_id, domain_type):
+        return app_id in self.entity_to_map and domain_type in self.entity_to_map[app_id]
+
+    def has_functions_map(self, app_id, map_name):
+        return app_id in self.functions_map and map_name in self.functions_map[app_id]
+
+    def has_model_cache(self, app_id, map_name):
+        return app_id in self.model_cache and map_name in self.model_cache[app_id]
--- Platform.App/python-template/mapper/transform.py
+++ Platform.App/python-template/mapper/transform.py
@@ -1,6 +1,69 @@
-from env.loader import Loader
+from mapper.index import Index
 from core.component import Component
+import json
 
 class Transform(Component):
-    def __init__(self):
+    def __init__(self, index):
         Component.__init__(self)
+        self.index = index
+
+    def replace_all(self, _from, needle, replacement):
+        """ replace all occurences in a string """
+        return _from.replace(needle, replacement)
+
+    def replace_all_atributes(self, _from, needle, replacement):
+        """ replace json attributes """
+        return self.replace_all(_from, "\""+needle+"\":", "\""+replacement+"\":")
+
+    def apply_runtime_fields(self, app_id, map_name, model_list):
+        """ apply runtime fields at query result """
+        accumulator = dict()
+        result = []
+        for model in model_list:
+            model_json = model.copy()
+            model_json['_metadata'] = dict()
+            model_json['_metadata']['type'] = map_name
+            self.apply_function_fields(
+                model_json, app_id, map_name, accumulator)
+            self.apply_metadata_fields(model_json)
+            result.append(model_json)
+        return result
+
+    def apply_function_fields(self, model_json, app_id, map_name, accumulator):
+        """ apply function fields """
+        if not self.index.has_functions_map(app_id, map_name):
+            return model_json
+
+        functions = self.index.get_functions(app_id, map_name)
+        for calc_prop in functions:
+            if not calc_prop in accumulator:
+                accumulator[calc_prop] = dict()
+            model_json[calc_prop] = eval(functions[calc_prop]['eval'], {"item":model_json, "accumulator":accumulator[calc_prop]})
+        return model_json
+
+    def apply_metadata_fields(self, model_json):
+        if "meta_instance_id" in model_json:
+            if model_json['meta_instance_id'] != None:
+                model_json['_metadata']['instance_id'] = model_json['meta_instance_id']
+            model_json.pop('meta_instance_id', None)
+        model_json['_metadata']['branch'] = "master"
+
+    def get_filters(self, app_id, map_name, query):
+        """ apply query filter on domain model """
+        filters = self.index.get_filters(app_id, map_name)
+        if filters == dict():
+            return filters
+        if not query["filter"] in filters:
+            return dict()
+        _filter = filters[query["filter"]]
+        _str = json.dumps(_filter)
+        for k in query.keys():
+            _str = self.replace_all(_str,":"+k, query[k])
+        map_fields = self.index.get_fields(app_id,map_name)
+        fields = map_fields.keys()
+        for k in fields:
+            if not 'column' in map_fields[k]:
+                continue
+            _str = self.replace_all(_str,k,map_fields[k]['column'])
+        return json.loads(_str)
+
