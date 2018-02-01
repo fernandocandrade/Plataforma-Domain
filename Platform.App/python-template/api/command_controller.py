cadb08390ebaa5f951ac466a54cfdd98076aabdd
Philippe Moneda <philippemoneda@gmail.com>
2018-02-01 13:23:41 -0200

    ajustes

--- Platform.App/python-template/api/command_controller.py
+++ Platform.App/python-template/api/command_controller.py
@@ -1,8 +1,9 @@
 """ Command Controller persist data on domain """
 class CommandController:
-    def __init__(self, app_id, request):
+    def __init__(self, app_id, request, mapper):
         self.app_id = app_id
         self.request = request
+        self.mapper = mapper
 
     def persist(self):
         """ Persist data on domain """
