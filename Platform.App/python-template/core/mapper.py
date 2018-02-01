09047176294249950bad764ba415a3f3d4b1698c
Fabio Gibson <fabiogibson@hotmail.com>
2018-02-01 20:33:12 -0200

    temporal models can be inserted and updated

--- Platform.App/python-template/core/mapper.py
+++ Platform.App/python-template/core/mapper.py
@@ -1,6 +1,7 @@
 import datetime as dt
 from uuid import uuid4
-from sqlalchemy import Column, Integer, String, DateTime, create_engine, orm, ForeignKey, event
+import itertools
+from sqlalchemy import Column, Integer, String, DateTime, create_engine, orm, ForeignKey, event, UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base, declared_attr
 from psycopg2 import extras as psql_extras
 import sqlalchemy.dialects.postgresql as sap
@@ -18,7 +19,7 @@ class ModelBase(object):
     def __tablename__(cls):
         return cls.__name__.lower()
 
-    id = Column(sap.UUID(as_uuid=True), primary_key=True, default=uuid4())
+    id = Column(sap.UUID(as_uuid=True), primary_key=True, default=uuid4)
 
     def save(self):
         self.objects.save(self)
@@ -55,34 +56,36 @@ class TemporalMixin:
     def get_history_name(cls, field):
         return f"{cls.get_classname()}{field.title()}History"
 
-    def get_entity_history(self, field, clock):
-        history_instance = globals()[self.get_history_name(field)]()
-        history_instance.value = getattr(self, field)
-        history_instance.entity = self
-        history_instance.ticks = '[1,]'
-        history_instance.clock = clock
-        clock.entity = self
-        return history_instance
-
-    def get_clock_entity(cls):
-        clock_entity = globals()[cls.get_clock_name()]()
-        clock_entity.effective = effective_now()
-        return clock_entity
 
 
 
 class User(Base, TemporalMixin):
-    objects = UserData
     temporal = ('name', )
     name = Column(String)
 
 
 class UserClock(Base):
-    ticks = Column(sap.INT4RANGE(), nullable=False, default='[1,)')
-    effective = Column(sap.TSTZRANGE(), nullable=False, default=effective_now)
+    ticks = Column(
+        Integer,
+        nullable=False,
+        default=0)
 
-    entity_id = Column(sap.UUID(as_uuid=True), ForeignKey('user.id'), nullable=False)
+    effective = Column(
+        sap.TSTZRANGE(),
+        nullable=False,
+        default=effective_now)
+
+    entity_id = Column(
+        sap.UUID(as_uuid=True),
+        ForeignKey('user.id'),
+        nullable=False)
+
     entity = orm.relationship("User")
+
+    __table_args__ = (
+        UniqueConstraint('entity_id', 'effective', name='_user_clock_uix'),
+    )
+
 
 class UserNameHistory(Base):
     clock = Column(ForeignKey(UserClock.id))
@@ -96,17 +99,72 @@ class UserNameHistory(Base):
     entity = orm.relationship("User")
 
 
+Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
+
+
+# TODO: Rename to get_or_create_entity_clock
+def get_or_create_clock_entity(self, period=effective_now()):
+    """Tries to retrieve an entity clock for the given period
+       from the database. If it does not exist, a new clock is
+       created.
+    """
+    clock_cls = globals()[self.get_clock_name()]
+
+    clock_entity = session.query(clock_cls)\
+        .filter(clock_cls.effective.contains(period), clock_cls.entity_id == self.id)\
+        .one_or_none()
+
+    if not clock_entity:
+        clock_entity = clock_cls()
+        clock_entity.effective = period
+        clock_entity.entity = self
+        clock_entity.ticks = 0
+        session.add(clock_entity)
+
+    return clock_entity
+
+
+def get_or_create_entity_history(self, field, clock):
+    new_value = getattr(self, field)
+    history_cls = globals()[self.get_history_name(field)]
+
+    history_entity = session.query(history_cls)\
+        .filter(
+            history_cls.entity_id == self.id,
+            history_cls.clock_id == clock.id,
+            history_cls.ticks.contains(clock.ticks),
+        ).one_or_none()
+
+    if history_entity:
+        if history_cls.value == new_value:
+            return history_entity, False
+
+        history_entity.ticks = f'[{history_entity.ticks.lower}, {clock.ticks}]'
+
+    new_history_entity = history_cls()
+    new_history_entity.entity = self
+    new_history_entity.clock = clock
+    new_history_entity.ticks = f'[{clock.ticks + 1},]'
+    new_history_entity.value = new_value
+    import ipdb; ipdb.set_trace()
+    session.add(new_history_entity)
+    return new_history_entity, True
 
 
 @event.listens_for(session, 'before_flush')
 def receive_before_flush(session, flush_context, instances):
-    for entity in session.new:
-        clock_entity = entity.get_clock_entity()
-        session.add(clock_entity)
+    for entity in itertools.chain(session.new, session.dirty):
+        if not entity.temporal:
+            continue
+
+        clock = get_or_create_clock_entity(entity)
+        changed = False
 
         for col in entity.temporal:
-            entity_history = entity.get_entity_history(col, clock_entity)
-            session.add(entity_history)
+            entity_history, created = get_or_create_entity_history(
+                entity, col, clock)
 
-
+            if not changed and created:
+                changed = True
+                clock.ticks += 1
