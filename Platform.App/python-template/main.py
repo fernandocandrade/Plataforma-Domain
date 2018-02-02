7cc78912a3b9a17b2300a4ac04bca373765751e1
Philippe Moneda <philippemoneda@gmail.com>
2018-02-02 17:31:53 -0200

    ajustes

--- Platform.App/python-template/main.py
+++ Platform.App/python-template/main.py
@@ -1,13 +1,6 @@
 ''' Main Module starts all domain app components '''
-import api.server as server
 from migration.sync import sync_db
-from flask_sqlalchemy import SQLAlchemy
-from settings.loader import Loader
-from model.domain import get_db_name
-env = Loader().load()
-db_host = env["database"]["host"]
-app = server.get_app()
-app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql+pyscopg2://postgres@{db_host}:5432/{get_db_name()}'
-db = SQLAlchemy(app)
-sync_db(db)
-server.run()
+from cross_cutting import db, app, get_db_name
+
+sync_db(db, get_db_name())
+app.run()
