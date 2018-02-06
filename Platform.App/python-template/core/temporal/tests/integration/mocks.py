0858371c25b4227f566c8c53d0d3ffb4f2cf003a
Fabio Gibson <fabiogibson@hotmail.com>
2018-02-06 14:45:13 -0200

    refactoring models

--- Platform.App/python-template/core/temporal/tests/integration/mocks.py
+++ Platform.App/python-template/core/temporal/tests/integration/mocks.py
@@ -10,54 +10,10 @@ Base = pytest.BaseModel
 
 
 class User(Base, TemporalModelMixin):
-    temporal = ('name', 'age', )
     name = Column(String)
     age = Column(Integer)
 
+    class Temporal:
+        fields = ('name', 'age', )
 
-#  class UserClock(Base):
-    #  ticks = Column(
-        #  Integer,
-        #  nullable=False,
-        #  default=0)
-#
-    #  effective = Column(
-        #  sap.TSTZRANGE(),
-        #  nullable=False,
-        #  default=effective_now)
-#
-    #  entity_id = Column(
-        #  sap.UUID(as_uuid=True),
-        #  ForeignKey('user.id'),
-        #  nullable=False)
-#
-    #  entity = orm.relationship("User")
-#
-    #  __table_args__ = (
-        #  UniqueConstraint('entity_id', 'effective', name='_user_clock_uix'),
-    #  )
-#
 
-#  class UserNameHistory(Base):
-    #  clock = Column(ForeignKey(UserClock.id))
-    #  ticks = Column(sap.INT4RANGE())
-    #  value = Column(String)
-#
-    #  clock_id = Column(sap.UUID(as_uuid=True), ForeignKey('userclock.id'), nullable=False)
-    #  clock = orm.relationship("UserClock")
-#
-    #  entity_id = Column(sap.UUID(as_uuid=True), ForeignKey('user.id'), nullable=False)
-    #  entity = orm.relationship("User")
-#
-#
-#  class UserAgeHistory(Base):
-    #  clock = Column(ForeignKey(UserClock.id))
-    #  ticks = Column(sap.INT4RANGE())
-    #  value = Column(Integer)
-#
-    #  clock_id = Column(sap.UUID(as_uuid=True), ForeignKey('userclock.id'), nullable=False)
-    #  clock = orm.relationship("UserClock")
-#
-    #  entity_id = Column(sap.UUID(as_uuid=True), ForeignKey('user.id'), nullable=False)
-    #  entity = orm.relationship("User")
-#
