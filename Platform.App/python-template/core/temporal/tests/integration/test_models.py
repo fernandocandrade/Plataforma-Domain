f3ebaf5ea529c12f83812bda388bda6c083399fc
Fabio Gibson <fabiogibson@hotmail.com>
2018-02-07 20:27:42 -0200

    tests refactoring

--- Platform.App/python-template/core/temporal/tests/integration/test_models.py
+++ Platform.App/python-template/core/temporal/tests/integration/test_models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import orm
 from core.temporal.tests.integration.mocks import User
 
 
@@ -17,7 +18,7 @@ def test_update_model_increases_model_clock(session, create_model, update_model,
     user = create_model(User, name='Foo', age=20)
     update_model(user, name='Bar')
 
-    # retrieve
+    # retrieve one and only one clock.
     clock = query_by_entity(User._clock, user.id).one()
 
     # assert
@@ -40,18 +41,18 @@ def test_create_new_model_creates_fields_history(session, create_model, query_by
     assert name_history.ticks.lower == 1
 
 
-def test_update_model_creates_new_field_history(session, create_model, update_model, query_by_entity):
+def test_update_model_creates_fields_history(session, create_model, update_model, query_entity_history):
     # act
-    user = create_model(User, name='Foo', age=20)
-    update_model(user, name='Bar')
+    user = create_model(User, name='Foo', age=20,)
+    update_model(user, name='Bar', age=10,)
 
     # retrieve
-    name_history = query_by_entity(
-        user._history['name'],
-        user.id,
-        user._history['name'].value).all()
+    name_history = query_entity_history(user, 'name')
+    age_history = query_entity_history(user, 'age')
 
     # assert
+    assert len(name_history) == 2
+
     assert name_history[0].value == 'Bar'
     assert name_history[0].ticks.lower == 2
     assert name_history[0].ticks.upper is None
@@ -59,6 +60,32 @@ def test_update_model_creates_new_field_history(session, create_model, update_mo
     assert name_history[1].value == 'Foo'
     assert name_history[1].ticks.lower == 1
     assert name_history[1].ticks.upper == 2
+
+    assert len(age_history) == 2
+
+    assert age_history[0].value == 10
+    assert age_history[0].ticks.lower == 2
+    assert age_history[0].ticks.upper is None
+
+    assert age_history[1].value == 20
+    assert age_history[1].ticks.lower == 1
+    assert age_history[1].ticks.upper == 2
+
+
+def test_update_model_does_not_duplicate_unchanged_fields(session, create_model, update_model, query_entity_history):
+    # act
+    user = create_model(User, name='Foo', age=20,)
+    update_model(user, name='Bar',)
+
+    # retrieve
+    age_history = query_entity_history(user, 'age')
+
+    # assert
+    assert len(age_history) == 1
+    assert age_history[0].value == 20
+    assert age_history[0].ticks.lower == 1
+    assert age_history[0].ticks.upper is None
+
 
 #
 #  def test_can_get_specific_tick():
@@ -68,8 +95,68 @@ def test_update_model_creates_new_field_history(session, create_model, update_mo
     #  assert query.count() == 1
 #
 #
-#  def test_can_join_entity_and_history():
+def test_can_retrieve_multiple_models(session, create_model, update_model):
+    # act
+    user = create_model(User, name='Foo', age=20,)
+    update_model(user, name="Bar",age=21)
+
+    #  models = session.query(*[v for v in User._history.values()]).all()
+    query = session.query(
+        User._history['name'].value,
+        User._history['age'].value,
+    ).join(
+        User._history['name'].entity_id,
+        User._history['age'].entity_id
+    ).filter(
+        User._history['name'].ticks.contains(1),
+        User._history['age'].ticks.contains(1)
+    )
+
+    #  for m in query.all():
+        #  print(m)
+
+    print(query)
+
+    #  for u, hn, ha in query:
+        #  print(u.id, hn.value, ha.value)
+
+    #  for m in query.all():
+        #  print(m.value)
+    #  models = session.query(
+        #  User._history['name'].value).\
+        #  options(orm.joinedload(
+            #  User._history['name'].entity, innerjoin=True))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  def test_can_join_entity_and_history(session):
+
 #
+    #  for k,v in User._history.items():
+        #  print(v)
+#
+    #  query = session.query([User] + [v for v in User._history.values()])
+    #  print(query)
+
     #  def build_sql(entity):
         #  cols = "public.user.id "
         #  joins = ""
@@ -89,7 +176,7 @@ def test_update_model_creates_new_field_history(session, create_model, update_mo
         #  {joins}
         #  where public.usernamehistory.ticks @> 2
     #  """)
-
+#
 
 #  def test_parse_dict_to_models():
     #  mocks = {
