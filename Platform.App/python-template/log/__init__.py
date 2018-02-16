ca4cfda35f4ac57c0680ea385f60aa53271dcec5
Philippe Moneda <philippemoneda@gmail.com>
2018-02-16 12:16:17 -0200

    adiciona metodo para desabilitar o log da aplicacao

--- Platform.App/python-template/log/__init__.py
+++ Platform.App/python-template/log/__init__.py
@@ -1,7 +1,7 @@
 import logging
 import sys
 
-root = logging.getLogger()
+root = logging.getLogger("app")
 root.setLevel(logging.DEBUG)
 
 ch = logging.StreamHandler(sys.stdout)
@@ -9,6 +9,9 @@ ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 root.addHandler(ch)
+
+def disable_log():
+    logging.getLogger("app").setLevel(logging.NOTSET)
 
 def debug(msg):
     logging.debug(msg)
