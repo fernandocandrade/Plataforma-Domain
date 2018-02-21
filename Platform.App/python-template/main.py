1d980baed171b0a6aa46223553aeb56d24b62ecf
Philippe Moneda <philippemoneda@gmail.com>
2018-02-21 17:09:58 -0300

    ajuste para sincronizar o banco com o gunicorm

--- Platform.App/python-template/main.py
+++ Platform.App/python-template/main.py
@@ -1,14 +1,4 @@
 ''' Main Module starts all domain app components '''
 from migration.sync import sync_db
-from utils.config_loader import load_config_file
-from api.server import app
-import log
-import logging
-logging.basicConfig()
-
-log.info("starting app")
 sync_db()
-log.info("starting api server")
-cnf = load_config_file()
-log.info(f"Server running on host=0.0.0.0 and port={cnf['http']['port']}")
-app.run(port=cnf["http"]["port"])
+
