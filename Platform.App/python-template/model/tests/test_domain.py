0b655f7586a35d25d71dc1c38f1c0871168dd21f
Fabio Gibson <fabiogibson@hotmail.com>
2018-03-01 14:34:57 -0300

    ignore envrc file

--- Platform.App/python-template/model/tests/test_domain.py
+++ Platform.App/python-template/model/tests/test_domain.py
@@ -2,12 +2,8 @@ from model.domain import conta
 from migration.sync import sync_db
 from database import create_session
 
-def test_save_conta():
-
-    c = conta()
-    session = create_session()
-    c.saldo = 1
-    c.titular = "Teste"
+def test_save_conta(session):
+    c = conta(saldo=1, titular="Teste")
     session.add(c)
     session.commit()
     assert session.query(conta).count() >= 1
