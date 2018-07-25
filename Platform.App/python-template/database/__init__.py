33b0d014201218f06bbe1e274bd1ad9b9104953a
Philippe Moneda <philippemoneda@gmail.com>
2018-07-25 20:36:11 -0300

    adicionar log do nome do banco

--- Platform.App/python-template/database/__init__.py
+++ Platform.App/python-template/database/__init__.py
@@ -8,18 +8,19 @@ from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy_utils import database_exists, create_database, drop_database
 from settings.loader import Loader
 from core.temporal.session import sessionmaker
-
+import log
 
 logging.getLogger('sqlalchemy.engine').setLevel(logging.FATAL)
 logging.getLogger('sqlalchemy.dialects').setLevel(logging.FATAL)
 logging.getLogger('sqlalchemy.pool').setLevel(logging.FATAL)
 logging.getLogger('sqlalchemy.orm').setLevel(logging.FATAL)
 
+
 conf = Loader().load()
 db_host = conf["database"]["host"]
 db_name = conf["app"]["name"]
 #db_name = domain.get_db_name()
-logging.info(db_name)
+log.info(db_name)
 db_user = conf["database"]["user"]
 conn_string = f'postgresql+psycopg2://{db_user}@{db_host}:5432/{db_name}'
 
