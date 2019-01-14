39c241794e3bcbc91723674049e714500694325d
Fernando Andrade <fernando.andrade@pitang.com>
2019-01-14 14:41:12 -0300

    Ajustes no CLI para atender todos projetos dotnet core e node

--- Platform.Cli/actions/presentation/dotnet/deployDotNetAppAction.js
+++ Platform.Cli/actions/presentation/dotnet/deployDotNetAppAction.js
@@ -27,7 +27,7 @@ module.exports = class DeployProcessAppAction extends BaseDeployAction {
 
     prepare(env) {
         var promise = new Promise((resolve, reject) => {
-            console.log("Preparing Deploy");
+            console.log("[.Net Temp Version] Preparing Deploy");
             var path = os.homedir() + "/installed_plataforma";
             var fullPath = path+"/apps/"+env.conf.app.version+"_"+env.conf.app.id;
             env.conf.fullPath = fullPath;
@@ -49,8 +49,8 @@ module.exports = class DeployProcessAppAction extends BaseDeployAction {
 
                 shell.rm("-rf", dest);
                 shell.mkdir("-p", dest);
-                shell.cp("-R",source+"/mapa", dest + "/mapa");
-                shell.cp("-R",source+"/metadados", dest + "/metadados");
+                shell.cp("-R",source+"/Mapa", dest + "/Mapa");
+                shell.cp("-R",source+"/Metadados", dest + "/Metadados");
                 shell.cp(source+"/Dockerfile", dest);
 
                 resolve(env);
@@ -67,11 +67,20 @@ module.exports = class DeployProcessAppAction extends BaseDeployAction {
                 var source = ".";
                 var dest = env.conf.fullPath;
                 let webAppName = env.conf.app.name;
+                let dockerName = env.conf.app.docker;
 
                 console.log("Publish Project");
 
-                shell.exec(`dotnet publish ${source}/server/${webAppName}.csproj -o ${dest}/server`);
-
+                if (fs.existsSync(`${source}/View`)) {
+                    shell.cd("View");
+                    shell.exec(`ng build --base-href /${dockerName}/ --deploy-url /${dockerName}/`);
+                    shell.cd("..");
+                }
+                
+                var cmdPublish = `dotnet publish ${source}/Server/${webAppName}.Web/${webAppName}.Web.csproj -o ${dest}/Server`; 
+                console.log(cmdPublish);
+                shell.exec(cmdPublish);
+                
                 resolve(env);
             } catch (e) {
                 reject(e);
@@ -81,11 +90,11 @@ module.exports = class DeployProcessAppAction extends BaseDeployAction {
     }
 
     uploadMaps(env) {
-        return this.getFiles(env, "mapa", (ctx, v) => this.saveMapToCore(ctx, v));
+        return this.getFiles(env, "Mapa", (ctx, v) => this.saveMapToCore(ctx, v));
     }
 
     uploadMetadata(env) {
-        var promise = this.getFiles(env, "metadados", (ctx, v) => this.processMetadata(ctx, v));
+        var promise = this.getFiles(env, "Metadados", (ctx, v) => this.processMetadata(ctx, v));
         return promise;
     }
 
@@ -155,7 +164,9 @@ module.exports = class DeployProcessAppAction extends BaseDeployAction {
             process.type = env.conf.app.type;
             process.relativePath = env.conf.fullPath;
             process.deployDate = new Date();
+            
             process.tag = this.docker.getContainer(env);
+            shell.cd(process.relativePath);
             this.docker.build(env, process.tag).then((r) => {
                 //console.log("Docker publish...");
                 //this.docker.publish(env, process.tag).then(() => {
@@ -167,6 +178,7 @@ module.exports = class DeployProcessAppAction extends BaseDeployAction {
                             env.variables = {};
                             env.variables["API_MODE"] = true;
                             env.variables["SYSTEM_ID"] = env.conf.solution.id;
+                            env.variables["PROCESS_ID"] = env.conf.app.id;
                             this.docker.run(env, process.tag).then(r => resolve(env));
                         });
                     }else{
